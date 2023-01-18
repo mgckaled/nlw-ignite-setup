d5e48e7bc1bb2b2015152e2aa8aecc8b3794601d
mgckaled <marcelgckaled@gmail.com>
2023-01-18 16:56:23 -0300

    [server]feat: server stage 2 done

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,18 +1,11 @@
 import cors from '@fastify/cors'
-import { PrismaClient } from '@prisma/client'
 import Fastify from 'fastify'
+import { appRoutes } from './routes'
 
 const app = Fastify()
-const prisma = new PrismaClient()
 
-// mecanismo de segurança - permite aplicações frontend buscar dados do backend
 app.register(cors)
-
-app.get('/', async () => {
-	const habits = await prisma.habit.findMany()
-
-	return habits
-})
+app.register(appRoutes)
 
 app
 	.listen({
